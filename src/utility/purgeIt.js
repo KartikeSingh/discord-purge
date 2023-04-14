3391456d6ec3dff73f7e0a603d4cf55df1a672aa
Xx-Mohit-xX <75924613+Xx-Mohit-xX@users.noreply.github.com>
2023-04-14 20:34:33 +0530

    updated code from djs v13 to v14

--- src/utility/purgeIt.js
+++ src/utility/purgeIt.js
@@ -15,8 +15,8 @@ module.exports = (f, message, channel, filter, number = 100, e) => {
         if (msgs.length === 0) {
             if (!handle) return rej({ type: "error", message: "No messages found", id: 3 });
 
-            if (message.replied) message.channel.send({ embeds: [{ color: "RED", title: `No messages found ${rejectEmoji}` }] })
-            else message.reply({ embeds: [{ color: "RED", title: `No messages found ${rejectEmoji}` }] })
+            if (message.replied) message.channel.send({ embeds: [{ color: "Red", title: `No messages found ${rejectEmoji}` }] })
+            else message.reply({ embeds: [{ color: "Red", title: `No messages found ${rejectEmoji}` }] })
 
             return res("done");
         }
@@ -27,8 +27,8 @@ module.exports = (f, message, channel, filter, number = 100, e) => {
 
         if (Object.keys(data).length > 0) for (let i = 0; i < Object.keys(data).length; i++)content += `**${Object.keys(data)[i]}** : ${Object.values(data)[i]}\n`
 
-        if (message.replied) message.channel.send({ embeds: [{ color: "GREEN", title: `Successfully Purged ${msgs.length} messages ${acceptEmoji}`, description: `Messages were from these users :\n` + content }] })
-        else message.reply({ embeds: [{ color: "GREEN", title: `Successfully Purged ${msgs.length} messages ${acceptEmoji}`, description: `Messages were from these users :\n` + content }] })
+        if (message.replied) message.channel.send({ embeds: [{ color: "Green", title: `Successfully Purged ${msgs.length} messages ${acceptEmoji}`, description: `Messages were from these users :\n` + content }] })
+        else message.reply({ embeds: [{ color: "Green", title: `Successfully Purged ${msgs.length} messages ${acceptEmoji}`, description: `Messages were from these users :\n` + content }] })
         res("done");
     })
 }
